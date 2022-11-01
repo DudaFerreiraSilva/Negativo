1dbd48b3fe72d373d9b51bed8f1c409588e2b03d
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-11-01 18:25:22 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int numero;
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.Write("Digite um número: ");
+
+numero = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+
+if (numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você digitou um número negativo.");
+    Console.ResetColor();
+}
